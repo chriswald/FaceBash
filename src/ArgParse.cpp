a4144b25b983c3a0dd2d5a3a5efc684e13e00378
Chris Wald <wald.j.chris@gmail.com>
2012-10-17 14:37:45 -0500

    Added comments to explain code (duh).

--- src/ArgParse.cpp
+++ src/ArgParse.cpp
@@ -31,30 +31,42 @@ ArgParse::~ArgParse()
  */
 void ArgParse::ParseArgs()
 {
+  // The user didn't enter any arguments. Just display the help text
+  // (also displays the version info).
   if (count == 0)
     {
       ShowHelpText();
       return;
     }
 
+  // The user specifically requested the help text. This also displays
+  // the version info.
   if      (argHas("-h") || argHas("--help"))
     ShowHelpText();
 
+  // The user wants to login to F.aceBash / Facebook.
   else if (argHas("-l") || argHas("--login"))
     Login();
 
+  // The user want to log out of F.aceBash / Facebook.
   else if (                argHas("--logout"))
     Logout();
 
+  // Show the user's news feed.
   else if (argHas("-n") || argHas("--show_news_feed"))
     ShowNewsFeed();
 
+  // Allow the user to post a status on either their own wall or the
+  // wall of a friend.
   else if (argHas("-s") || argHas("--update_status"))
     UpdateStatus();
 
+  // Show the version information (without help text).
   else if (argHas("-v") || argHas("--version"))
     ShowVersion();
 
+  // The user entered some argument that isn't supported or
+  // recognized. Show the help text (with version information).
   else
     ShowHelpText();
 }
@@ -95,20 +107,24 @@ void ArgParse::Login()
   int status;
   pid_t pid = fork();
   
-  if (pid < 0)
+  if (pid < 0)        // Fork failed for some reason
     {
-      perror("fork error");
+      perror("Fork error");
     }
-  else if (pid == 0)
+  else if (pid == 0)  // Child process
     {
       execl("/usr/bin/python2.7", "/usr/bin/python2.7", "scripts/login.py", login.user().c_str(), login.pass().c_str(), (char *) 0);
     }
-  else
+  else                // Parent process
     {
       if ((pid = wait(&status)) == -1)
 	perror("wait error");
     }
   
+  // Checks to see whether the login was successful by open the
+  // member27 file and checking its contents. If something that looks
+  // like an authentification token is found that's good. Otherwise
+  // display a message relating to the error that was found.
   std::ifstream member27;
   member27.open("member27");
   if (member27.is_open())
@@ -122,27 +138,33 @@ void ArgParse::Login()
 	}
       else
 	{
+	  /*
+	   * Error Conditions:
+	   *  1) The user entered an invalid username (email address)
+	   *     or password when logging in.
+	   *  2) The user did not grant permission to F.aceBash to
+	   *     access their account (eg selected no instead of yes
+	   *     when asked to grant permission).
+	   *  3) The user is not connected to the internet.
+	   */
 	  if      (strcmp(content.c_str(), "1") == 0)
-	    {
 	      cout << "Invalid Email or Password." << endl;
-	    }
+
 	  else if (strcmp(content.c_str(), "2") == 0)
-	    {
 	      cout << "User denied permission to F.aceBash." << endl;
-	    }
+
 	  else if (strcmp(content.c_str(), "3") == 0)
-	    {
 	      cout << "Not connected to the Internet." << endl;
-	    }
-	  else
-	    {
+
+	  else // Something went wrong (maybe on Facebook's end)
 	      cout << "An unknown error occured." << endl;
-	    }
+
 	}
     }
-  else
+  else // The file could not be opened. Something went wrong on this end
     {
-      cerr << "Unable to login." << endl;
+      cerr << "Unable to login. Make sure you have proper "
+	   << "local permissions." << endl;
     }
 
   member27.close();
@@ -166,36 +188,37 @@ void ArgParse::Logout()
  */
 void ArgParse::UpdateStatus()
 {
+  // Try to find a friend whose name matches what is being searched
+  // for.
   string who;
   if (count > 1)
     {
       who = getFriendID(arguments[1]);
       if (strcmp(who.c_str(), "\0") == 0)
 	{
+	  // Found more or less than one friend with that name?
+	  // Just return.
 	  return;
 	}
     }
   else
     {
+      // No name passed as a parameter?
+      // Default to the current user.
       who = "me";
-    }
-
-  string token = authToken();
-  if (strcmp(token.c_str(), "\0") == 0)
-    {
-      cout << "Please Log in." << endl;
-      return;
     }
 
   stringstream ss;
   string message = prompt(string("Status: "));
   string url = string("https://graph.facebook.com/"+who+"/feed");
   cURLpp::Forms formParts;
-  formParts.push_back(new cURLpp::FormParts::Content("access_token", token));
   formParts.push_back(new cURLpp::FormParts::Content("message", message));
 
   bool request_success = makeRequest(ss, url, formParts);
 
+  // If the request wasn't successfully made just return. Some
+  // function previous to this should have displayed some error
+  // message.
   if (!request_success)
     {
       return;
@@ -228,6 +251,9 @@ void ArgParse::ShowNewsFeed()
   string url = string("https://graph.facebook.com/me/home");
   bool request_success = makeRequest(ss, url);
 
+  // If the request wasn't successfully made just return. Some
+  // function previous to this should have displayed some error
+  // message.
   if (!request_success)
     {
       return;
@@ -253,7 +279,11 @@ void ArgParse::ShowNewsFeed()
   unsigned int how_many;
   if (count > 1)
     {
+      // Converts the first argument to an integer
       how_many = atoi(arguments[1].c_str());
+
+      // If the requested number of posts is out of range then just
+      // set it to the maximum available.
       if (how_many <= 0 || how_many > posts.size())
 	{
 	  how_many = posts.size();
@@ -261,31 +291,46 @@ void ArgParse::ShowNewsFeed()
     }
   else
     {
+      // If no number of posts is specified by the user just set it to
+      // the maximum available.
       how_many = posts.size();
     }
 
   stack<Json::Value> stories;
 
+  // Keep track of how many posts have actually been shown, not just
+  // how many to try to show.
   unsigned int how_many_have_been_shown = 0;
+
   for (unsigned int i = 0; i < posts.size(); i ++)
     {
       string content_type = posts[i]["type"].asString();
+
+      // Make sure the post is a status and isn't empty
       if (strcmp(content_type.c_str(), "status") == 0 &&
 	  strlen(posts[i]["message"].asString().c_str()) > 0)
 	{
+	  // Push the story onto a stack to display later.
 	  stories.push(posts[i]);
-
+	  // Then increment how many stories "have been shown". The
+	  // stories will actually be shown later.
 	  how_many_have_been_shown ++;
+
+	  // If the number of accepted stories reaches the number of
+	  // stories available break out of the loop.
 	  if (how_many_have_been_shown >= how_many)
 	    break;
 	}
     }
   
-  int index = 1;
+  // While there are stories still on the stack...
   while (!stories.empty())
     {
+      // Get the top story and pass it for formatting along with an
+      // index number that can be used for referencing it later. Then
+      // remove it from the stack.
       Json::Value story = stories.top();
-      formatNewsStory(cout, story, index++);
+      formatNewsStory(cout, story, how_many_have_been_shown--);
       stories.pop();
     }
   
@@ -352,9 +397,7 @@ string ArgParse::prompt(string message)
 /*
  * Get Auth Token:
  * Attempts to retrieve the auth token. On success the auth token is
- * returned. On failure the user is prompted. The token is again
- * polled for. On success the token is returned. On failure a message
- * is displayed and NULL is returned.
+ * returned. On failure \0 is returned.
  */
 string ArgParse::authToken()
 {
@@ -382,6 +425,7 @@ string ArgParse::authToken()
  */
 string ArgParse::getFriendID(string name)
 {
+  // Query Facebook for a list of all the user's friends.
   stringstream ss;
   string url = string("https://graph.facebook.com/me/friends");
   bool request_success = makeRequest(ss, url);
@@ -391,10 +435,13 @@ string ArgParse::getFriendID(string name)
       return "\0";
     }
 
+  // Parse said list.
   Json::Value root;
   Json::Reader reader;
 
   bool parsingSuccessful = reader.parse(ss.str(), root);
+
+  // Make sure no errors occurred.
   if (!parsingSuccessful)
     {
       cerr << "Failed to parse the document." << endl;
@@ -413,6 +460,8 @@ string ArgParse::getFriendID(string name)
 
   vector<string> ids (0);
 
+  // Search throught the returned friend list for all names matching
+  // the name parameter.
   const Json::Value data = root["data"];
   for (unsigned int i = 0; i < data.size(); i ++)
     {
@@ -422,6 +471,8 @@ string ArgParse::getFriendID(string name)
 	}
     }
 
+  // If the number of matches is not exactly one display an error
+  // message and return \0. Otherwise return the ID of the one match.
   if (ids.size() > 1)
     {
       cerr << "You have more than one friend with that name." << endl;
@@ -448,12 +499,16 @@ void ArgParse::formatNewsStory(ostream & os, Json::Value story, int index)
   string message = story["message"].asString();
   string posted_by = story["from"]["name"].asString();
 
+  // This conditional is probably unnecessarry since the same
+  // condition is being checked in ShowNewsFeed, but its also not
+  // hurting anything so I'll keep it.
   if (strlen(message.c_str()) > 0)
     {
       vector<string> lines;
       string remainder = message;
       
       vector<string> new_lines;
+      // Break the message body at newlines.
       int nl_index = remainder.find("\n");
       while (nl_index != -1 && nl_index != (int) strlen(remainder.c_str()))
 	{
@@ -464,6 +519,8 @@ void ArgParse::formatNewsStory(ostream & os, Json::Value story, int index)
 	}
       new_lines.push_back(remainder);
       
+      // Run through each of those lines and make sure it does not
+      // exceed LINE_WIDTH. Split it into sub lines until it does.
       for (unsigned int j = 0; j < new_lines.size(); j ++)
 	{
 	  string new_line = new_lines[j];
@@ -477,6 +534,7 @@ void ArgParse::formatNewsStory(ostream & os, Json::Value story, int index)
 	  lines.push_back(new_line);
 	}
 
+      // Write each individual portion of the story.
       writeSeperatorLine(os);
       writeNameLine(os, posted_by, index);
       writeSeperatorLine(os);
@@ -486,6 +544,11 @@ void ArgParse::formatNewsStory(ostream & os, Json::Value story, int index)
     }
 }
 
+/*
+ * Write Message Line:
+ * Writes each line from lines to os with leading characters and
+ * trailing characters with enough spaces to evenly align everything.
+ */
 void ArgParse::writeMessageLines(ostream & os, const vector<string> lines)
 {
   for (unsigned int i = 0; i < lines.size(); i ++)
@@ -500,6 +563,12 @@ void ArgParse::writeMessageLines(ostream & os, const vector<string> lines)
     }
 }
 
+/*
+ * Write Name Line:
+ * Writes the name (left aligned) and index (right aligned) to os. The
+ * two values are written with a fixed width of LINE_WIDTH. Leading
+ * and trailing characters are also printed.
+ */
 void ArgParse::writeNameLine(ostream & os, const string & name, const int index)
 {
   stringstream ss;
@@ -513,7 +582,11 @@ void ArgParse::writeNameLine(ostream & os, const string & name, const int index)
   os << ss.str();
 }
   
-
+/*
+ * Write Seperator Line:
+ * Writes a line of width LINE_WIDTH used as a filler / seperator
+ * between elements.
+ */
 void ArgParse::writeSeperatorLine(ostream & os)
 {
   stringstream ss;
@@ -541,11 +614,20 @@ int ArgParse::showErrorMessage(const Json::Value & root)
   return 0;
 }
 
+/*
+ * ReLogin:
+ * Uuuhhhhhh, yeah this might do something at some point.
+ */
 bool ArgParse::relogin()
 {
   return false;
 }
 
+/*
+ * Make Request:
+ * Makes a request to the given url and writes the return to
+ * ss. Returns true if the request was successful, false otherwise.
+ */
 bool ArgParse::makeRequest(stringstream & ss, const string & url)
 {
   string token = authToken();
@@ -573,6 +655,12 @@ bool ArgParse::makeRequest(stringstream & ss, const string & url)
   return true;
 }
 
+/*
+ * Make Request:
+ * Makes a request to the given url with the given form data and
+ * writes the return to ss. Returns true if the request was
+ * successful, false otherwise.
+ */
 bool ArgParse::makeRequest(stringstream & ss, const string & url, const cURLpp::Forms & formParts)
 {
   string token = authToken();
