408bbb7343fa0e7e1781fd09877a554bc4c15ff6
Chris Wald <wald.j.chris@gmail.com>
2012-11-13 08:40:24 -0800

    Added photos permissions.

--- scripts/login.py
+++ scripts/login.py
@@ -16,7 +16,7 @@ browser = mech.Browser()
 uri =  'https://www.facebook.com/dialog/oauth?'
 uri += 'client_id=103806206443210'
 uri += '&redirect_uri=http://chriswald.com/facebash.htm'
-uri += '&scope=user_about_me,friends_about_me,friends_birthday,user_likes,friends_likes,user_relationships,friends_relationships,user_status,friends_status,publish_stream,read_stream'
+uri += '&scope=user_about_me,friends_about_me,friends_birthday,user_likes,friends_likes,user_relationships,friends_relationships,user_status,friends_status,publish_stream,read_stream,user_photos,friends_photos'
 uri += '&response_type=token'
 
 # Pretend I'm not a bot by creating a cookie cache and setting the
