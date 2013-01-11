cb51a95d272045861fa0b8a4611efc8ff206711f
Chris Wald <wald.j.chris@gmail.com>
2013-01-11 08:05:01 -0800

    Added notifications permission.

--- scripts/login.py
+++ scripts/login.py
@@ -50,7 +50,7 @@ def main():
     uri =  'https://www.facebook.com/dialog/oauth?'
     uri += 'client_id=103806206443210'
     uri += '&redirect_uri=http://chriswald.com/facebash.htm'
-    uri += '&scope=user_about_me,friends_about_me,friends_birthday,user_likes,friends_likes,user_relationships,friends_relationships,user_status,friends_status,publish_stream,read_stream,user_photos,friends_photos,user_videos,friends_videos'
+    uri += '&scope=user_about_me,friends_about_me,friends_birthday,user_likes,friends_likes,user_relationships,friends_relationships,user_status,friends_status,publish_stream,read_stream,user_photos,friends_photos,user_videos,friends_videos,manage_notifications'
     uri += '&response_type=token'
 
     # Pretend I'm not a bot by creating a cookie cache and setting the
